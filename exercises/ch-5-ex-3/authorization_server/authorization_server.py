4194c7be4e6064607c5cc098b27a927c4fcf32d4
carbone84 <bryanacarbone@gmail.com>
2020-11-15 12:55:52 -0500

    ch-5-ex-3 initial setup

---
+++ exercises/ch-5-ex-3/authorization_server/authorization_server.py
@@ -0,0 +1,197 @@
+from flask import Flask
+from flask import render_template, redirect, request
+import secrets, base64
+from tinydb import TinyDB, Query
+
+app = Flask(__name__)
+
+db = TinyDB('../database.json')
+
+codes = {}
+requests = {}
+
+# authorization server information
+auth_server = {
+    'authorization_endpoint': 'http://localhost:5001/authorize',
+    'token_endpoint': 'http://localhost:5001/token'
+}
+
+# client information
+clients = [
+    {
+        'client_id': 'oauth-client-1',
+        'client_secret': 'oauth-client-secret-1',
+        'redirect_uris': ['http://localhost:5000/callback']
+        #
+        # Add a set of allowed scopes for this client
+        #
+    }
+]
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', clients=clients, auth_server=auth_server)
+
+@app.route('/authorize', methods=['GET'])
+def authorize():
+    client = getClient(request.args.get('client_id', ''))
+    
+    if not client:
+        print(f"Unknown client {request.args.get('client_id', '')}")
+        return render_template('error.html', error="Unknown client")
+    elif request.args.get('redirect_uri', '') not in client['redirect_uris']:
+        print(f"Mismatched redirect URI, expected {client.redirect_uris} got {request.args.get('redirect_uri', '')}")
+        return render_template('error', error="Invalid redirect URI")
+    else:
+        #
+        # Validate that the set of scopes the client is requesting
+        # aligns with the set of scopes the client is registered for.
+        #
+        request_id = secrets.token_urlsafe(8)
+        requests[request_id] = request.args
+
+        #
+        # Send the requested scopes to the approval page for rendering
+        #
+        return render_template('approve.html', client=client, request_id=request_id)
+    
+
+@app.route('/approve', methods=['GET', 'POST'])
+def approve():
+    request_id = request.form.get('request_id')
+    query = requests[request_id]
+    del requests[request_id]
+
+    if not query:
+        return render_template('error.html', error="No matching authorization request")
+    
+    if request.form.get('approve'):
+        if query['response_type'] == 'code':
+            #
+            # Make sure the approved scopes from the form are allowed for this client
+            #
+            code = secrets.token_urlsafe(8)
+
+            #
+            # Save the approved scopes as part of this object
+            #
+            codes[code] = { 'request': query}
+            
+            # look into url.parse in js>py
+            callback_url = query['redirect_uri'] + f"?code={code}&state={query['state']}"
+            return redirect(callback_url)
+        else:
+            error = "unsupported_response_type"
+            callback_url = query['redirect_uri'] + f"?error={error}"
+            return redirect(callback_url)
+    else:
+        error = "access_denied"
+        callback_url = query['redirect_uri'] + f"?error={error}"
+        return redirect(callback_url)
+
+@app.route('/token', methods=['POST'])
+def token():
+    auth = request.headers.get('authorization')
+    client_id = None
+    if auth:
+        client_credentials_b64 = auth[8:len(auth)-1].encode()
+        client_credentials_bytes = base64.b64decode(client_credentials_b64)
+        client_credentials = client_credentials_bytes.decode('ascii').split(':')
+        client_id = client_credentials[0]
+        client_secret = client_credentials[1]
+
+    if request.form.get('client_id', ''):
+        if client_id:
+            print("Client attempted to authenticate with multiple methods")
+            return "invalid_client", 401
+        
+        client_id = request.args.get['client_id']
+        client_secret = request.args.get['client_secret']
+
+    client = getClient(client_id)
+
+    if not client:
+        print(f"Unknown client {client_id}")
+        return "invalid_client", 401
+    if client['client_secret'] != client_secret:
+        print(f"Mismatched client secret, expected {client.client_secret} got {client_secret}")
+        return "invalid_client", 401
+    if request.form.get('grant_type') == 'authorization_code':
+        code = codes[request.form.get('code')]
+        if code:
+            del codes[request.form.get('code')]
+            if code['request']['client_id'] == client_id:
+                #
+                # Save the approved scopes as part of the token's structure
+                #
+                access_token = secrets.token_urlsafe(32)
+                refresh_token = secrets.token_urlsafe(32)
+
+                #insert to db
+                db.insert({
+                    'access_token': access_token,
+                    'client_id': client_id
+                })
+                db.insert({
+                    'refresh_token': refresh_token,
+                    'client_id': client_id
+                })
+                print(f"Issuing access token {access_token}")
+                
+                #
+                # Return scopes as part of the token response
+                #
+                
+                token_response = {
+                    'access_token': access_token,
+                    'token_type': 'Bearer'
+                }
+
+                print(f"Issued tokens for code {request.form.get('code')}")
+                return token_response, 200
+            else:
+                print(f"Client mismatch, expected {code['authorization_endpoint_request']['client_id']} got {client_id}")
+                return "invalid_grant", 400
+        else:
+            print(f"Unknown code, {request.args.get('code')}")
+            return "invalid_grant", 400
+    elif request.form.get('grant_type') == 'refresh_token':
+        #call db to check for refresh token
+        sql = Query()
+        tokens = db.search(sql.refresh_token == request.form.get('refresh_token'))
+        if len(tokens) == 1:
+            token = tokens[0]
+            if token['client_id'] != client_id:
+                print(f"Invalid client using a refresh token, expected {token['client_id']} got {client_id}")
+                db.remove(sql.refresh_token == request.form.get('refresh_token'))
+                return 400
+            print(f"We found a matching refresh token: {request.form.get('refresh_token')}")
+            #
+            # Bonus: handle scopes for a refresh token request appropriately
+            #
+            access_token = secrets.token_urlsafe(32)
+            token_response = {
+                    'access_token': access_token,
+                    'token_type': 'Bearer',
+                    'refresh_token': token['refresh_token']
+                }
+            db.insert({
+                    'access_token': access_token,
+                    'client_id': token['client_id']
+                })
+            print(f"Issuing access token {access_token} for refresh token {request.form.get('refresh_token')}")
+            return token_response, 200
+        else:
+            print("No matching token was found.")
+            return 'invalid_grant', 400
+    else:
+        print(f"Unknown grant type, {request.args.get('grant_type')}")
+        return "unsupported_grant_type", 400
+
+def getClient(client_id):
+    for client in clients:
+        if client['client_id'] == client_id:
+            return client
+    return "Client not found"
+
+db.truncate()
---
+++ exercises/ch-5-ex-3/client/client.py
@@ -0,0 +1,156 @@
+from flask import Flask
+from flask import render_template, redirect, request, session, url_for
+import secrets, urllib.parse, base64, requests
+
+app = Flask(__name__)
+
+# need a secret key for session variables
+app.secret_key = secrets.token_urlsafe(16)
+
+# authorization server information
+auth_server = {
+  'authorization_endpoint': 'http://localhost:5001/authorize',
+  'token_endpoint': 'http://localhost:5001/token',
+  'revocation_endpoint': 'http://localhost:5001/revoke',
+  'registration_endpoint': 'http://localhost:50001/register',
+  'userinfo_endpoint': 'http://localhost:5001/userinfo'
+}
+
+rsa_key = {
+  'alg': 'RS256',
+  'e': 'AQAB',
+  'n': 'p8eP5gL1H_H9UNzCuQS-vNRVz3NWxZTHYk1tG9VpkfFjWNKG3MFTNZJ1l5g_COMm2_2i_YhQNH8MJ_nQ4exKMXrWJB4tyVZohovUxfw-eLgu1XQ8oYcVYW8ym6Um-BkqwwWL6CXZ70X81YyIMrnsGTyTV6M8gBPun8g2L8KbDbXR1lDfOOWiZ2ss1CRLrmNM-GRp3Gj-ECG7_3Nx9n_s5to2ZtwJ1GS1maGjrSZ9GRAYLrHhndrL_8ie_9DS2T-ML7QNQtNkg2RvLv4f0dpjRYI23djxVtAylYK4oiT_uEMgSkc4dxwKwGuBxSO0g9JOobgfy0--FUHHYtRi0dOFZw',
+  'kty': 'RSA',
+  'kid': 'authserver'
+}
+
+# client information
+client = {
+  'client_id': 'oauth-client-1',
+  'client_secret': 'oauth-client-secret-1',
+  'redirect_uris': ['http://localhost:5000/callback'],
+  'scope': 'foo bar'
+}
+
+protected_resource = 'http://localhost:5002/resource'
+
+@app.route('/')
+def index():
+  session['state'] = ''
+  session['access_token'] = ''
+  session['refresh_token'] = ''
+  session['scope'] = ''
+  return render_template('index.html', access_token=session['access_token'], scope=session['scope'], refresh_token=session['refresh_token'])
+
+@app.route('/authorize')
+def authorize():
+  session['access_token'] = ''
+  session['refresh_token'] = ''
+  session['state'] = secrets.token_urlsafe(32)
+  session['scope'] = ''
+    
+  authorize_url = buildUrl(auth_server['authorization_endpoint'], {
+    'response_type': 'code',
+    'client_id': client['client_id'],
+    'redirect_uri': client['redirect_uris'][0],
+    'state': session['state']
+  })
+  print(f"redirect: {authorize_url}")
+  return redirect(authorize_url)
+
+@app.route('/callback')
+def callback():
+  if request.args.get('error', ''):
+    return render_template('error.html', error=request.args.get('error', ''))
+
+  response_state = request.args.get('state', '')
+  if response_state != session['state']:
+    print(f"State DOES NOT MATCH: expected {session['state']} got {response_state}")
+    return render_template('error.html', error="State value did not match")
+
+  code = request.args.get('code', '')
+
+  form_data = {
+    'grant_type': 'authorization_code',
+    'code': code,
+    'redirect_uri': client['redirect_uris'][0]
+  }
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': f"Basic {encodeClientCredentials(client['client_id'], client['client_secret'])}"
+  }
+  token_response = requests.post(auth_server['token_endpoint'], data=form_data, headers=headers)
+  print(f"Requesting access token for code {code}")
+
+  if token_response.status_code >= 200 and token_response.status_code < 300:
+    body = token_response.json()
+    session['access_token'] = body['access_token']
+    print(f"Got access token: {session['access_token']}")
+    if body.get('refresh_token'):
+      session['refresh_token'] = body['refresh_token']
+      print(f"Got refresh token: {session['refresh_token']}")
+    session['scope'] = body.get('scope')
+    print(f"Got scope: {session['scope']}")
+    return render_template('index.html', access_token=session['access_token'], scope=session['scope'], refresh_token=session['refresh_token'])
+  else:
+    return render_template('error.html', error=f"Unable to fetch access token, server response: {token_response.status_code}")
+  
+@app.route('/refresh')
+def refresh():
+  form_data = {
+    'grant_type': 'refresh_token',
+    'refresh_token': session['refresh_token'],
+    'client_id': client['client_id'],
+    'client_secret': client['client_secret'],
+    'redirect_uri': client['redirect_uris'][0]
+  }
+  print(f"form_data: {form_data}")
+  headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+  print(f"Refreshing token {session['refresh_token']}")
+
+  token_response = requests.post(auth_server['token_endpoint'], data=form_data, headers=headers)
+  if token_response.status_code >= 200 and token_response.status_code <300:
+    body = token_response.json()
+    session['access_token'] = body['access_token']
+    print(f"Got access token: {session['access_token']}")
+    if body.get('refresh_token'):
+      session['refresh_token'] = body['refresh_token']
+      print(f"Got refresh token: {session['refresh_token']}")
+    session['scope'] = body.get('scope')
+    print(f"Got scope: {session['scope']}")
+    return render_template('index.html', access_token=session['access_token'], refresh_token=session['refresh_token'], scope=session['scope'])
+  else:
+    print("No refresh token, asking the user to get a new access token")
+    return render_template('index.html', access_token=session['access_token'], refresh_token=session['refresh_token'], scope=session['scope'])
+
+@app.route('/fetch_resource')
+def fetch_resource():
+  if not session.get('access_token'):
+    return render_template('error.html', error="Missing Access Token")
+
+  print(f"Making request with access token {session['access_token']}")
+
+  headers = {
+    'Authorization': f"Bearer {session['access_token']}",
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  resource = requests.post(protected_resource, headers=headers)
+
+  if resource.status_code >= 200 and resource.status_code < 300:
+    body = resource.json()
+    return render_template('data.html', resource=body)
+  else:
+    session['access_token'] = ''
+    return render_template('error.html', error=f"Server returned response code: {resource.status_code}")
+
+def buildUrl(base, options):
+  url = urllib.parse.urlsplit(base)
+  query_string = urllib.parse.urlencode(options)
+  new_url = urllib.parse.urlunsplit((url.scheme, url.netloc, url.path, query_string, ""))
+  return new_url
+
+def encodeClientCredentials(client_id, client_secret):
+  credentials = urllib.parse.quote(client_id, safe='') + ':' + urllib.parse.quote(client_secret, safe='')
+  credentials_bytes = credentials.encode('ascii')
+  credentials_b64 = base64.b64encode(credentials_bytes)
+  return credentials_b64
