2f3bc80a7978bcee051bc2a19f37a39931ec0a0d
carbone84 <bryanacarbone@gmail.com>
2020-11-25 18:39:42 -0500

    completed ch-6-ex-1

--- exercises/ch-6-ex-1/protected_resource/protected_resource.py
+++ exercises/ch-6-ex-1/protected_resource/protected_resource.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import render_template, request, g
+from flask import render_template, request, g, make_response
 from tinydb import TinyDB, Query
+from functools import wraps
 from time import time
 
 app = Flask(__name__)
@@ -12,27 +13,36 @@ protected_resource = {
     'description': 'This data has been protected by OAuth 2.0'
 }
 
-@app.before_request
-def before_request():
-    g.access_token = getAccessToken()
-    if g.access_token:
-        print(f"Found access token {g.access_token}")
-    else:
-        return "Error", 401
+def prerequisites(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if request.method == 'POST':
+            g.access_token = getAccessToken()
+            if not g.access_token:
+                return "Error", 401
+            print(f"Found access token {g.access_token}")
+            return f(*args, **kwargs)
+        elif request.method == 'OPTIONS':
+            return build_cors_preflight_response()
+    return decorated_function
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/resource', methods=['POST'])
+@app.route('/resource', methods=['POST', 'OPTIONS'])
+@prerequisites
 def resource():
     if g.access_token:
-        return protected_resource
+        response = make_response(protected_resource)
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
     else:
         return "Error", 401
 
 def getAccessToken():
     auth = request.headers.get('authorization')
+    print(f"AUTH: {auth}")
     in_token = ''
     if auth and auth.lower().index('bearer') == 0:
         in_token = auth[7:len(auth)]
@@ -51,3 +61,10 @@ def getAccessToken():
     else:
         print("No matching token was found.")
         return
+
+def build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    # response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
